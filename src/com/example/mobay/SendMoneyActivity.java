dd8ca463965dd8e46a6e0f56e586ff7186e7d800
apautrat <apautrat@test.com>
2015-01-05 19:38:35 +0100

    Activite SendMoneyActivity: Petites modifs.

--- src/com/example/mobay/SendMoneyActivity.java
+++ src/com/example/mobay/SendMoneyActivity.java
@@ -20,30 +20,25 @@ import com.example.model.Mobay;
 import com.example.model.Operation;
 import com.example.model.TypeOperation;
 import com.example.model.Utilisateur;
+import com.parse.ParseObject;
 // soit rajouter un champ à la table operation pour valider un operation quand l'autre la recoit+valide après ça on débite le premier
 // operation doit avoir un champ de destinataire sinon comment on fait pour connaître qui on doit donner l'argent
 	// comment on fait si un user a plusieur demande d'argent a accepter ??? une par une , on fait une liste
-public class SendMoneyActivity extends Activity {
-
+public class SendMoneyActivity extends Activity 
+{
 	private static final String TAG = "SendMoneyActivity";
 
 	Button valider = null;
 	Button menuAccueil = null;
 	Button deconnexion = null;
-
-	private static EditText numOrAlias = null;
-	private static EditText montant = null;
-
-	private static Boolean numero = false;
-	private static Boolean alias = false;
-
-	String numOrAliasText = null;
-	String montantText = null;
-
-	static Boolean isUser = false;
+	static EditText numOrAlias = null;
+	EditText montant = null;
+	
+	static Utilisateur utilisateurIndique = null;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_send_money);
 
@@ -56,9 +51,11 @@ public class SendMoneyActivity extends Activity {
 		numOrAlias.addTextChangedListener(textWatcher);
 		montant.addTextChangedListener(textWatcher);
 
-		deconnexion.setOnClickListener(new View.OnClickListener() {
+		deconnexion.setOnClickListener(new View.OnClickListener() 
+		{
 			@Override
-			public void onClick(View v) {
+			public void onClick(View v) 
+			{
 				Intent deconnexion = new Intent(SendMoneyActivity.this, MainMenuActivity.class);
 				startActivity(deconnexion);
 			}
@@ -66,22 +63,19 @@ public class SendMoneyActivity extends Activity {
 		});
 		menuAccueil.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v) {
+			public void onClick(View v) 
+			{
 				Intent deconnexion = new Intent(SendMoneyActivity.this, MainMenuActivity.class);
 				startActivity(deconnexion);
 			}
 
 		});
-
 		valider.setOnClickListener(validerListener);
-
 	}
 
-	private TextWatcher textWatcher = new TextWatcher() {// détecter les
-															// caracteres
-															// spéciaux avec
-															// regexs
-
+	// détecter les caracteres spéciaux avec regexs
+	private TextWatcher textWatcher = new TextWatcher() 
+	{
 		@Override
 		public void afterTextChanged(Editable s) {
 			// TODO Auto-generated method stub
@@ -99,141 +93,107 @@ public class SendMoneyActivity extends Activity {
 			// TODO Auto-generated method stub
 
 		}
-
 	};
 
-	private OnClickListener validerListener = new OnClickListener() {
+	private OnClickListener validerListener = new OnClickListener() 
+	{
 		@Override
-		public void onClick(View v) {
-
-			String numOrAliasText = null;
-			String montantText = null;
-
-			numOrAliasText = SendMoneyActivity.getValueField(numOrAlias.getText().toString());
-			montantText = SendMoneyActivity.getValueField(montant.getText().toString());
+		public void onClick(View v) 
+		{
+			String numOrAliasText = numOrAlias.getText().toString();
+			String montantText = montant.getText().toString();
 
 			Log.d(TAG, "Texte du champ NumOrAlias: " + numOrAliasText);
 			Log.d(TAG, "Texte du champ Montant: " + montantText);
 
-			if (numOrAliasText != null) {
-				if (montantText != null) {
-					if (numOrAlias.length() < 4) {
-						numOrAlias.getText().clear();
-						Toast.makeText(getBaseContext(), "Le champ \"Num. Mobile ou Alias\" doit contenir 4 caractères au minimum", Toast.LENGTH_SHORT).show();
-					} else if (numOrAlias.length() > 20) {
-						numOrAlias.getText().clear();
-						Toast.makeText(getBaseContext(), "Le champ \"Num. Mobile ou Alias\" ne doit pas dépasser 20 caractères", Toast.LENGTH_SHORT).show();
-					} else {
-						double dbl = Double.parseDouble(montantText);
-						Log.d(TAG, "Montant parse en double : " + dbl);
-						if (dbl > 0) {
-							SendMoneyActivity.isUserExistsInDB(numOrAliasText);
-							Log.d(TAG, "User est-il dans la db ? : " + isUser);
-							if (isUser) {
-								isUser = false;
-								if (numero && !(((Utilisateur) Utilisateur.getUtilisateurWithAttribut("numTel", numOrAliasText).get(0)).getObjectId().equals(Mobay.utilisateurCourant.getObjectId()))) {
-									Log.d(TAG, "Envoi d'argent : OK");
-									numero = false;
-									SendMoneyActivity.envoiMoney(dbl);
-									Intent sendMoneyOk = new Intent(SendMoneyActivity.this, SendMoneyOkActivity.class);
-									startActivity(sendMoneyOk);
-								}
-								if (alias && !(((Utilisateur) Utilisateur.getUtilisateurWithAttribut("pseudo", numOrAliasText).get(0)).getObjectId().equals(Mobay.utilisateurCourant.getObjectId()))) {
-									alias = false;
-									Log.d(TAG, "Envoi d'argent : OK");
-									SendMoneyActivity.envoiMoney(dbl);
-									Intent sendMoneyOk = new Intent(SendMoneyActivity.this, SendMoneyOkActivity.class);
-									startActivity(sendMoneyOk);
-								} else {
-									numOrAlias.getText().clear();
-									montant.getText().clear();
-									Toast.makeText(getBaseContext(), "Vous ne pouvez pas vous envoyer de l'argent à vous même", Toast.LENGTH_SHORT).show();
-								}
-							} else {
-								Log.d(TAG, "Envoi d'argent : NOK");
-								Intent sendMoneyNok = new Intent(SendMoneyActivity.this, SendMoneyNokActivity.class);
-								startActivity(sendMoneyNok);
+			// Verifications
+			// Numero / alias absent
+			if (numOrAliasText.isEmpty()) 
+			{
+				Toast.makeText(getBaseContext(), "Merci de rentrer un numéro de téléphone ou un alias!", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			
+			// Montant absent
+			if (montantText.isEmpty()) 
+			{
+				Toast.makeText(getBaseContext(), "Merci de rentrer un montant!", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			
+			// Num / alias < 4 caracteres et > 20 caracteres
+			if (numOrAlias.length() < 4 || numOrAlias.length() > 20) 
+			{
+				Toast.makeText(getBaseContext(), "Le champ \"Num. Mobile ou Alias\" doit contenir entre 4  et 20 caractères!", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			
+			double montant = Double.parseDouble(montantText);
+			Log.d(TAG, "Montant parse en double : " + montant);
+			// Montant < 0
+			if (montant <= 0) 
+			{
+				Toast.makeText(getBaseContext(), "Le montant doit être supérieur à zéro!", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			
+			// Pas d'utilisateur avec ce pseudo ou ce num
+			if(Utilisateur.getUtilisateurWithAttribut("numTel", numOrAliasText).isEmpty()
+				&& Utilisateur.getUtilisateurWithAttribut("pseudo", numOrAliasText).isEmpty())
+			{
+				Log.d(TAG, "User est pas dans la db !");
+				Log.d(TAG, "Envoi d'argent : NOK");
+				// La, c'est vraiment abusé de foutre une nouvelle activité..
+				Intent sendMoneyNok = new Intent(SendMoneyActivity.this, SendMoneyNokActivity.class);
+				startActivity(sendMoneyNok);
+				return;
+			}
+			
+			Log.d(TAG, "User est bien dans la db !");
+			
+			// On recupere l'utilisateur avec le num ou l'alias indique
+			if(!Utilisateur.getUtilisateurWithAttribut("numTel", numOrAliasText).isEmpty())
+				utilisateurIndique = (Utilisateur) Utilisateur.getUtilisateurWithAttribut("numTel", numOrAliasText).get(0);
+			
+			if(!Utilisateur.getUtilisateurWithAttribut("pseudo", numOrAliasText).isEmpty())
+				utilisateurIndique = (Utilisateur) Utilisateur.getUtilisateurWithAttribut("pseudo", numOrAliasText).get(0);
 
-							}
-						} else {
-							montant.getText().clear();
-							Toast.makeText(getBaseContext(), "Le montant doit être supérieur à zéro", Toast.LENGTH_SHORT).show();
-						}
-					}
-				} else {
-					Toast.makeText(getBaseContext(), "Merci de rentrer un montant", Toast.LENGTH_SHORT).show();
-				}
-			} else {
-				Toast.makeText(getBaseContext(), "Merci de rentrer un numéro de téléphone ou un alias", Toast.LENGTH_SHORT).show();
+			if(utilisateurIndique.getObjectId().matches(Mobay.utilisateurCourant.getObjectId()))
+			{
+				Toast.makeText(getBaseContext(), "Vous ne pouvez pas vous envoyer de l'argent à vous même", Toast.LENGTH_SHORT).show();
+				return;
 			}
 
+			Log.d(TAG, "Envoi d'argent : OK");
+			SendMoneyActivity.envoiMoney(montant);
+			Intent sendMoneyOk = new Intent(SendMoneyActivity.this, SendMoneyOkActivity.class);
+			startActivity(sendMoneyOk);
 		}
-
 	};
 
-	private static String getValueField(String str) {
-
-		if (str == null || str.trim().length() == 0) {
-			return null;
-		} else {
-			return str;
-		}
-	}
-
-	private static void isUserExistsInDB(String str) {
-
-		List listUser = null;
-		Log.d(TAG, "Valeur de str dans isUserExistsInDB : " + str);
-		if (str.matches("^\\d+$")) {
-
-			listUser = Utilisateur.getUtilisateurWithAttribut("numTel", str);
-			Log.d(TAG, "Valeur de listeUser : " + listUser.toString());
-			if (!listUser.isEmpty()) {
-				numero = true;
-				isUser = true;
-			}
-		} else {
-
-			listUser = Utilisateur.getUtilisateurWithAttribut("pseudo", str);
-
-			if (!listUser.isEmpty()) {
-				alias = true;
-				isUser = true;
-			}
-
-		}
-
-	}
-
-	private static void envoiMoney(double dbl) {
-
-		List listAccount = null;
+	private static void envoiMoney(double dbl) 
+	{
+		List<ParseObject> listAccount = null;
 		double solde = 0;
-		String numOrAliasText = SendMoneyActivity.getValueField(numOrAlias.getText().toString());
-		// on ne doit pas pouvoir s'envoyer de l'argent a soi même
-		Log.d(TAG, "ObjectId de l'utilisateur courant : " + Mobay.utilisateurCourant.getObjectId());
-		Log.d(TAG, "ObjectId de l'utilisateur specifie par le champ : " + ((Utilisateur) Utilisateur.getUtilisateurWithAttribut("numTel", numOrAliasText).get(0)).getObjectId());
 
 		Operation op = new Operation(Mobay.utilisateurCourant.getObjectId(), TypeOperation.ENVOI, dbl, new Date());
 		op.saveInBackground();
 		// on recupere le compte de l'utilisateur courant
 		listAccount = Compte.getAccountWithUserObjectId(Mobay.utilisateurCourant.getObjectId());
 
-		if (listAccount.size() == 0) {
-
+		if (listAccount.size() == 0) 
+		{
 			Compte cmpt = new Compte(Mobay.utilisateurCourant.getObjectId(), 0);
 			Log.d(TAG, "Création d'un compte pour l'utilisateur courant");
-
-		} else {
+		} else 
+		{
 			solde = (double) ((Compte) listAccount.get(0)).getSolde();
 			Log.d(TAG, "Solde avant operation : " + solde);
 
 			solde += dbl;
 			((Compte) listAccount.get(0)).setSolde(solde);
-
 			((Compte) listAccount.get(0)).saveInBackground();
-
 		}
-
 	}
 }
+
